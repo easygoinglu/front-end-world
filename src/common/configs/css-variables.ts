5cc35a36d00ea2068cf803be42d1728e64079ad9
easygoinglu <easygoinglu@gmail.com>
2022-05-14 01:09:58 +0800

    feat: add css z-index config

--- src/common/configs/css-variables.ts
+++ src/common/configs/css-variables.ts
@@ -1,4 +1,10 @@
 export default {
     breakpoint_medium: '48rem', // 768px * 62.5% / 10px
     breakpoint_large: '75rem', // 1200px * 62.5% / 10px
+    
+    layer_content_bottom: -1,
+    layer_content_top: 1,
+    layer_layout_bottom: 4, // header
+    layer_layout_middle: 5, // mask
+    layer_layout_top: 6, // modal & menu
 };
