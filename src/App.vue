958577e3424a2d9d56498c7a9a5bbfb94d3e1bb8
easygoinglu <easygoinglu@gmail.com>
2022-05-14 01:37:36 +0800

    feat: add layout

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <TheLayout />
+</template>
+
+<script setup lang="ts">
+import TheLayout from '@/layout/Layout.vue';
+</script>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="wrapper">
+        <header class="header">
+            <TheMenu />
+        </header>
+        <main class="main">
+            <router-view
+                v-slot="{Component}"
+            >
+                <Transition
+                    name="fade"
+                    mode="out-in"
+                >
+                    <component
+                        :is="Component"
+                        :key="$route.path"
+                    />
+                </Transition>
+            </router-view>
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+import TheMenu from '@/components/TheMenu.vue';
+</script>
+
+<style lang="postcss" scoped>
+.header {
+    position: fixed;
+    z-index: $layer_layout_bottom;
+    width: 100%;
+    background-color: #1d1f42;
+}
+
+.main {
+    margin: 0 auto;
+
+    @media not all and (min-width: $breakpoint_medium) {
+        padding: 7rem 1.5rem 1.5rem;
+    }
+
+    @media (min-width: $breakpoint_medium) {
+        max-width: 96rem;
+        padding-top: 10rem;
+    }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity .3s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
