8ae90a2b96c76125e7bf341506a238c822009698
easygoinglu <easygoinglu@gmail.com>
2022-05-15 07:42:24 +0800

    feat: add pinia plugin for store

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import AppLink from '@/components/AppLink.vue'
 import router from '@/router';
+import store from '@/store';
 import 'normalize.css';
 import '@/css/base.css';
 
 createApp(App)
     .component('AppLink', AppLink)
     .use(router)
+    .use(store)
     .mount('#app');
