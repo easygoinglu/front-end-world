82e69fb0b1c7628b96db5b4a48c80ce1e5772b47
easygoinglu <easygoinglu@gmail.com>
2022-05-16 12:26:51 +0800

    feat: add button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,37 @@
+<template>
+    <button
+        class="button"
+        type="button"
+        :name="name"
+        @click="action"
+    >
+        <slot />
+    </button>
+</template>
+                
+<script setup lang="ts">
+defineProps<{
+    name?: string,
+}>();
+
+const emit = defineEmits<{
+    (e: 'action'): void,
+}>();
+
+const action = () => {
+    emit('action');
+};
+</script>
+
+<style scoped lang="postcss">
+.button {
+    border: .1rem solid $color_gray_light_secondary;
+    padding: 1rem 2rem;
+    font-weight: 600;
+    font-size: 1.8rem;
+    color: $color_black_light;
+    background-color: $color_white;
+    border-radius: .7rem;
+    cursor: pointer;
+}
+</style>
