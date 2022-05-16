949abb29e6ef6ed15ec9d17d8db2592e1f8cecc2
easygoinglu <easygoinglu@gmail.com>
2022-05-16 11:14:16 +0800

    feat: add radar chart component

---
+++ src/components/AppChartRadar.vue
@@ -0,0 +1,56 @@
+<template>
+    <Radar
+        :chart-options="chartOptions"
+        :chart-data="chartData"
+        :chart-id="chartId"
+        :width="width"
+        :height="height"
+        :css-classes="cssClasses"
+        :styles="styles"
+        :plugins="plugins"
+    />
+</template>
+
+<script setup lang="ts">
+import { Radar } from 'vue-chartjs';
+import {
+    Chart,
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    RadialLinearScale,
+    LineElement,
+    ChartData,
+    Plugin,
+} from 'chart.js';
+
+Chart.register(
+    Title,
+    Tooltip,
+    Legend,
+    PointElement,
+    RadialLinearScale,
+    LineElement,
+);
+
+interface Props {
+    chartOptions: object;
+    chartData: ChartData<'radar', (number | null)[], unknown>;
+    chartId?: string;
+    width?: number;
+    height?: number;
+    cssClasses?: string;
+    styles?: object | undefined;
+    plugins?: Plugin<'radar'>[];
+}
+
+withDefaults(defineProps<Props>(), {
+    chartId: 'radar-chart',
+    width: 400,
+    height: 400,
+    cssClasses: '',
+    styles: undefined,
+    plugins: () => [],
+});
+</script>
