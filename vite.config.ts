df8dd13cd605bb05e4c8b27cc8d0ecb4392d47ac
easygoinglu <easygoinglu@gmail.com>
2022-05-23 07:47:06 +0800

    feat: add image compression plugin

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import postcssNested from 'postcss-nested';
 import postcssFor from 'postcss-for';
 import postcssVars from 'postcss-simple-vars';
 import cssVariables from './src/common/configs/css-variables';
+import viteImagemin from 'vite-plugin-imagemin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,36 @@ export default defineConfig({
             '@': path.resolve(__dirname, './src'),
         },
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        viteImagemin({
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 20,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
+            svgo: {
+                plugins: [
+                    {
+                        name: 'removeViewBox',
+                    },
+                    {
+                        name: 'removeEmptyAttrs',
+                        active: false,
+                    },
+                ],
+            },
+        }),
+    ],
     css: {
         postcss: {
             plugins: [
